8fd699b06c30fdb49b4b9ed00108fc084f83aec7
seano424 <soreilly424@gmail.com>
2023-09-12 14:36:59 -0500

    cleaning up

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,22 +1,17 @@
-import Link from 'next/link'
-
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import SidebarNavbar from './components/SidebarNavbar'
 
 export default function DashboardLayout({
-  children, // will be a page or nested layout
+  children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <section className='flex min-h-screen flex-col scroll-smooth'>
-      {/* Include shared UI here e.g. a header or sidebar */}
       <Navbar />
       <SidebarNavbar />
       <Sidebar />
-      {/* max-width: calc(100vw - 5rem); */}
-      {/* "w-[calc(100%+2rem)]"" */}
       <main className='relative top-36 min-h-screen bg-gray-50  lg:ml-80'>
         <div className='container'>
           {children}
