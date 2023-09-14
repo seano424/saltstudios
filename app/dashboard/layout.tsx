a2a93850346e54ba0f33070cf1ed985fc2a3b388
seano424 <soreilly424@gmail.com>
2023-09-14 11:12:42 -0500

    beginning of sidebar updates

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import SearchModal from './components/SearchModal'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import SidebarNavbar from './components/SidebarNavbar'
 
 export default function DashboardLayout({
   children,
@@ -12,7 +11,6 @@ export default function DashboardLayout({
     <section className='flex min-h-screen flex-col scroll-smooth'>
       <SearchModal />
       <Navbar />
-      <SidebarNavbar />
       <Sidebar />
       <main className='dark:bg-theme-dark relative top-28 min-h-screen bg-gray-50 dark:text-theme-soft-peach lg:ml-80'>
         <div className='container py-5'>
---
+++ components/SidebarItems.tsx
@@ -0,0 +1,89 @@
+import clsx from 'clsx'
+
+interface SidebarItemProps {
+  name: string
+  icon: 'page' | 'elements' | 'page-example'
+  items: {
+    name: string
+    href: string
+  }[]
+}
+
+export default function SidebarItems({
+  name,
+  icon,
+  items,
+}: SidebarItemProps) {
+  return (
+    <div>
+      <button
+        className={clsx(
+          'flex items-center gap-1 rounded px-4 py-3 transition-colors duration-200 hover:bg-gray-50 focus:bg-gray-50 dark:text-theme-soft-peach dark:hover:bg-sky-500 dark:focus:bg-sky-500'
+          // pathname === '/dashboard'
+          //   ? 'bg-blue-50/50 font-bold text-sky-500 dark:bg-sky-400'
+          //   : 'font-medium'
+        )}
+      >
+        {icon === 'page' && (
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path
+              stroke='none'
+              d='M0 0h24v24H0z'
+              fill='none'
+            ></path>
+            <path d='M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z'></path>
+            <path d='M10 4l4 16'></path>
+            <path d='M12 12l-8 2'></path>
+          </svg>
+        )}
+        {icon === 'elements' && (
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path d='M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z'></path>
+            <path d='M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2'></path>
+            <path d='M12 12l0 .01'></path>
+            <path d='M3 13a20 20 0 0 0 18 0'></path>
+          </svg>
+        )}
+        {icon === 'page-example' && (
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path d='M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z'></path>
+            <path d='M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2'></path>
+            <path d='M12 12l0 .01'></path>
+            <path d='M3 13a20 20 0 0 0 18 0'></path>
+          </svg>
+        )}
+        <span>{name}</span>
+      </button>
+    </div>
+  )
+}
